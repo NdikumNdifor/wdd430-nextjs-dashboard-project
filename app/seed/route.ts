226699f18193732bf2b9b9ec069da39e6357875a
Ndikum Sabastine Ndifor <sebastienndifor@gmail.com>
2026-05-21 23:16:46 +0100

    Complete database setup and data fetching.

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -112,6 +112,8 @@ export async function GET() {
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
+    // console.error("=== SEEDING CRASH LOG ===", error);
+    // return new Response(error.message || String(error), { status: 500 });
     return Response.json({ error }, { status: 500 });
   }
 }
