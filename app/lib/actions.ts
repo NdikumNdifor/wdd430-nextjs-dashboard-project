6104005b9289a6af6d2fb5bba73848f477f76f0f
Ndikum Sabastine Ndifor <sebastienndifor@gmail.com>
2026-05-25 15:25:24 +0100

    Add next-auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
@@ -91,10 +94,10 @@ export async function updateInvoice(id: string, formData: FormData) {
   `;
   }catch(error){
     console.error(error)
-    // throw new Error('Database Error: Failed to Update Invoice.');
-    return {
-        message: "Database Error: Failed to delete invoice",
-    };
+    throw new Error('Database Error: Failed to Update Invoice.');
+    // return {
+    //     message: "Database Error: Failed to delete invoice",
+    // };
   }
   
  
@@ -106,4 +109,25 @@ export async function deleteInvoice(id: string) {
     throw new Error('Failed to Delete Invoice');
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+
+// For auth
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
