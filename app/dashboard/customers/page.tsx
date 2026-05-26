856beff5c3a54ec2a54ab84e05057908df0cc1b6
Ndikum Sabastine Ndifor <sebastienndifor@gmail.com>
2026-05-26 13:22:04 +0100

    Updated customer and invoice creation

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next';
+ 
+// Custom specific page mete data
+export const metadata: Metadata = {
+  // title: 'Invoices | Acme Dashboard',
+  title: 'Customers',
+};
+
+
 export default function Page() {
   return <p>Customers Page</p>;
 }
--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,14 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
+
+import { Metadata } from 'next';
+ 
+// Custom specific page mete data
+export const metadata: Metadata = {
+  // title: 'Invoices | Acme Dashboard',
+  title: 'Create Invoice',
+};
  
 export default async function Page() {
   const customers = await fetchCustomers();
