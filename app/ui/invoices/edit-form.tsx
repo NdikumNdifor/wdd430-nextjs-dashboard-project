1d9196ae463c4199b287d8890e8f93dcf238e9de
Ndikum Sabastine Ndifor <sebastienndifor@gmail.com>
2026-05-26 14:08:45 +0100

    Fixed type mismatch error

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,9 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInvoice } from '@/app/lib/actions';
+import { updateInvoice, State } from '@/app/lib/actions';
+
+import { useActionState } from 'react';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,9 +20,14 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+  // const initialState: State = { message: null, errors: {} };
+  // const [state, formAction] = useActionState(updateInvoice, initialState);
+  // const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -33,6 +40,7 @@ export default function EditInvoiceForm({
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -44,6 +52,15 @@ export default function EditInvoiceForm({
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          {/* Added error checking for customer field, Notice, placed after the customers div */}
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+               <p className="mt-2 text-sm text-red-500" key={error}>
+                 {error}
+               </p>
+             ))}
           </div>
         </div>
 
